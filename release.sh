ebba12c7e96a1f60b411f0746c43e15fc3d236c2
ketikai <ketikai@idealstate.team>
2025-06-13 10:58:39 +0800

    chore(build): 修正发布脚本，添加发布脚本到 .dockerignore
    Signed-off-by: ketikai <ketikai@idealstate.team>

--- release.sh
+++ release.sh
@@ -15,9 +15,11 @@ fi
 # docker system prune -f
 
 docker build -t ketikai/github-webhook-rust:$1 .
+
 docker push ketikai/github-webhook-rust:$1
 
 docker tag ketikai/github-webhook-rust:$1 ketikai/github-webhook-rust:latest
+
 docker push ketikai/github-webhook-rust:latest
 
 echo "Successfully released version '$1'"
